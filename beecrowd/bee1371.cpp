c7b486a0caea1167b7d0df45071fbc2adb52a5d1
ian <ian.ribeiro4@gmail.com>
2026-06-03 12:32:43 -0300

    update

--- beecrowd/bee1371.cpp
+++ beecrowd/bee1371.cpp
@@ -2,7 +2,7 @@
  * Contest : Beecrowd
  * Problem : Fechem as Portas!
  * Link    : https://judge.beecrowd.com/pt/problems/view/1371
- * Time    : O(???)
+ * Time    : O(√N)
  */
 
 #include <bits/stdc++.h>
@@ -14,22 +14,13 @@ using ll = long long;
 int main() {
   fastio
 
-  const int N = 25000000+1;
-  vector<int> crivo(N, 1);
-  for (int i = 2; i < N; ++i) {
-    if (crivo[i] == 1) {
-      for (int j = i; j < N; j += i) {
-        crivo[j] += ;
-      }
-    }
-  }
-
   int n;
   while (cin >> n and n) {
-    cout << "1 ";
-
-    for (int x = 2; x <= n; ++x) {
-
+    bool first = true;
+    for (ll i = 1; i*i <= n; ++i) {
+      if (!first) cout << ' ';
+      cout << i*i;
+      first = false;
     }
     cout << '\n';
   }
