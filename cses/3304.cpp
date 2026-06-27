6b70bb8f19b593f33cfa25f304c1d6d8a496a47f
ian <ian.ribeiro4@gmail.com>
2026-06-26 23:16:50 -0300

    update

--- cses/3304.cpp
+++ cses/3304.cpp
@@ -11,12 +11,9 @@ using ll = long long;
 
 #define fastio ios::sync_with_stdio(0); cin.tie(0);
 
-
 struct SegTree {
   struct Node {
-    int cnt = 1, max_h = 0;
-
-    Node(int cnt, int h) : cnt(cnt), max_h(h) {}
+    int cnt = 0, max_h = 0;
   };
 
   vector<int> v;
@@ -25,11 +22,11 @@ struct SegTree {
 
   SegTree(int n) : n(n) {
     v.assign(4*n, 0);
-    tree.assign(4*n, Node(1,0));
+    tree.assign(4*n, Node{});
   }
 
   Node join(const Node& a, const Node& b) {
-    Node ab(a.cnt, 0);
+    Node ab{a.cnt, 0};
     if (a.max_h < b.max_h) ab.cnt += b.cnt;
     ab.max_h = max(a.max_h, b.max_h);
     return ab;
@@ -37,7 +34,7 @@ struct SegTree {
 
   void build(int no, int l, int r) {
     if (l == r) {
-      tree[no] = Node(1,v[l]);
+      tree[no] = Node{1,v[l]};
       return;
     }
     int m = l + (r-l)/2;
@@ -48,8 +45,11 @@ struct SegTree {
   }
 
   Node query(int no, int l, int r, int ql, int qr) {
-    if (l > qr || r < ql) return Node(0,0);
-    if (l >= ql && r <= qr) return tree[no];
+    if (l > qr || r < ql) return Node{0,0};
+    if (l >= ql && r <= qr) {
+      fprintf(stderr, "range[%i..%i] | tree[%i] = { %i , %i }\n", l, r, no,  tree[no].cnt, tree[no].max_h);
+      return tree[no];
+    }
     int m = l + (r-l)/2;
 
     return join(query(2*no, l, m, ql, qr), query(2*no + 1, m + 1, r, ql, qr));
@@ -59,17 +59,23 @@ struct SegTree {
 int main() {
   fastio
 
-  /*
-   4  1  2  2  3
-  +4 -3 +1  0 +1
+  int n, q;
+  cin >> n >> q;
 
-  Quantas vezes de [l..r] o valor
-  de max atualiza
+  SegTree st(n);
+  for (int i = 1; i <= n; ++i) cin >> st.v[i];
+  st.build(1, 1, n);
 
+  for (int i = 0; i < st.tree.size(); ++i) {
+    cout << i << ": " << st.tree[i].cnt << ' ' << st.tree[i].max_h << endl;
+  }
 
-
-
-  */
+  while (q--) {
+    int a, b;
+    cin >> a >> b;
+    cout << st.query(1, 1, n, a, b).cnt << '\n';
+    cerr << '\n';
+  }
 
   return 0;
 }
